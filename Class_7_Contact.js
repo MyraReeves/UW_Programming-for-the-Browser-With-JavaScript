f408a02e4ebebd56a74ecfad879054fa340a6671
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-11-24 04:06:08 -0800

    Validates message length and reason for contacting

--- Class_7_Contact.js
+++ Class_7_Contact.js
@@ -3,8 +3,13 @@ const userName = document.getElementById('name');
 const email = document.getElementById('email');
 const contactReason = document.getElementById('contact-kind');
 const job = document.getElementById('job');
-// const website = document.getElementById('website');
+const jobTitle = document.getElementById('jobTitle');
+const jobError = document.getElementById('jobError');
+const urlError = document.getElementById('urlError');
+const website = document.getElementById('website');
 const language = document.getElementById('language');
+const codingSelection = document.getElementById('coding-language');
+const languageError = document.getElementById('language-error');
 const message = document.getElementById('message');
 
 
@@ -24,6 +29,7 @@ const nameLength = (input, min) => {
         return false;
     }
 };
+// _____________________________________________________________
 
 
 // Function to check if email matches regex  /\w+@\w+\.\w+/
@@ -41,7 +47,79 @@ const validateEmail = (input) => {
         document.getElementById("emailError").style.visibility = 'visible'
         return false;
     }
-}
+};
+// _________________________________________________________________
+
+
+// Function to ensure a job title is provided:
+const jobTitleValidation = () => {
+    if (jobTitle.value){
+        jobError.style.visibility = 'hidden';
+        jobTitle.parentElement.classList.remove('invalid');
+    }
+    else {
+        jobTitle.parentElement.classList.add('invalid');
+        jobError.style.visibility = 'visible';
+    }
+};
+
+// Function to ensure a company website is provided
+// and that url is valid for regex of /https?\:\/\/.+\..+/ 
+const websiteValidation = () => {
+    const websiteRegEx = /https?\:\/\/.+\..+/;
+    if (website.value){
+        if (websiteRegEx.test(website.value)){
+            // If true, remove invalid class on the parent of the element:
+            website.parentElement.classList.remove('invalid');
+            // and hide urlError message:
+            urlError.style.visibility = 'hidden';
+        }
+        else {
+            website.parentElement.classList.add('invalid');
+            urlError.style.visibility = 'visible';
+        }
+    }
+    else {
+        // If false, set invalid class on the parent of the element and show a message to the user
+        website.parentElement.classList.add('invalid');
+        urlError.style.visibility = 'visible';
+    }
+};
+
+// Validates that coding language is changed:
+const languageSelection = () => {
+    console.log('A coding language was selected!');
+    languageError.style.visibility = 'hidden';
+};
+
+// Function to hide/show additional info under selection choice:
+const selectionChoices = (selectedElement) => {
+    const selectedValue = selectedElement.value;
+    if (selectedValue === 'job') {
+        job.classList.remove('hiddenOption');
+        language.classList.add('hiddenOption');
+        jobError.style.visibility = 'visible';
+        urlError.style.visibility = 'visible';
+        jobTitle.parentElement.classList.add('invalid');
+        website.parentElement.classList.add('invalid');
+        job.addEventListener('input', jobTitleValidation);
+        website.addEventListener('input', websiteValidation);
+    }
+    else if (selectedValue === 'coding'){
+        language.classList.remove('hiddenOption');
+        job.classList.add('hiddenOption');
+        languageError.style.visibility = 'visible'; //Shows message if no change
+        codingSelection.addEventListener('change', languageSelection)
+    }
+};
+
+// Call above function upon any change to the contactReason field:
+contactReason.addEventListener('change', () => {
+    selectionChoices(contactReason);
+});
+
+
+// _____________________________________________________________________
 
 // Function to check for minimum length of message:
 const messageLength = (input, min) => {
@@ -58,36 +136,20 @@ const messageLength = (input, min) => {
         return false;
     }
 };
-
-// Function to hide/show additional info under selection choice:
-const selectionChoices = (selectElement) => {
-    const selectedValue = selectElement.value;
-    if (selectedValue === 'job') {
-        job.classList.remove('hiddenOption');
-        language.classList.add('hiddenOption');
-    }
-    else if (selectedValue === 'coding'){
-        language.classList.remove('hiddenOption');
-        job.classList.add('hiddenOption');
-    }
-};
-
-contactReason.addEventListener('change', () => {
-    selectionChoices(contactReason);
-});
+// ________________________________________________________________
 
 
 theFormContainer.addEventListener('submit', (eventObject) => {
     console.log('Form submission attempted...');
     if(
         // Check Name should be 3 characters or more
-        nameLength(userName, 3) &&      // shorthand for if this === true and
+        nameLength(userName, 3) &&
 
         // Email should match regex /\w+@\w+\.\w+/
-        validateEmail(email) &&          // ...if this === true
+        validateEmail(email) &&
 
         // Check Message should be 10 characters or more
-        messageLength(message, 10)       // ...if this === true and...
+        messageLength(message, 10)
     ){
         console.log('GOOD form')
     }
@@ -97,4 +159,4 @@ theFormContainer.addEventListener('submit', (eventObject) => {
         eventObject.preventDefault();
         console.log("BAD INPUT");
     }
-})
+});
