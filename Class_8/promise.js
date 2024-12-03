cdc03e416719f2bfd6423d278aa9f0d82bb4e4d1
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-02 16:49:05 -0800

    Solved assignment

---
+++ Class_8/promise.js
@@ -0,0 +1,36 @@
+/* • Create a new promise. The function passed to the promise 
+should:
+ • After 1 second, call Math.random()
+ • If the result of Math.random() is > 0.5, call resolve()
+ • If the result of Math.random() is <= 0.5, call reject()
+ • If the promise is resolved, should console.log('success')
+ • If the promise is rejected
+ , should console.log('fail')
+ • In either case, should console.log('complete')
+*/
+
+// Create the promise:
+const randomNumberPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomVal = Math.random()
+  
+      if (randomVal > 0.5){
+        resolve(randomVal)
+      }
+      else {
+        reject(new Error('The value was NOT greater than 0.5!'));
+      }
+    }, 1000)
+  });
+  
+  
+  // Call the promise:
+  randomNumberPromise.then((returnedValue) => {
+    console.log("SUCCESS! Random value is:", returnedValue)
+    return returnedValue
+  }).catch((error) => {
+    console.log("FAIL!  \nThe following are details about why it failed:\n", error);
+  }).finally(() => {
+    console.log('Complete')      
+  });
+  
