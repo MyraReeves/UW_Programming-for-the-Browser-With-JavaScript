3642003ecaa00587d285664bd38dd6c320d05eff
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-02 18:12:24 -0800

    Adds assignment instructions

--- Class_8/remove-notice.js
+++ Class_8/remove-notice.js
@@ -1,3 +1,5 @@
+// There is a site maintenance notice being displayed on our site.  Remove the notice after 5 seconds (we'll assume the user should have had a chance to read by that point).
+
 const siteNotice = document.getElementById('maintenance-notice');
 
 setTimeout(function() {
