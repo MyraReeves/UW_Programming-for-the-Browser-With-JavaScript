2eb7193df4863f2f0ebc101aa255fe6548e3940d
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-02 18:15:25 -0800

    Adds instructions to top

--- Class_8/hello-darkness.js
+++ Class_8/hello-darkness.js
@@ -1,3 +1,11 @@
+/*Using setInterval:
+1. Every half a second, darken the body backgroundColor by 1
+2. Stop (clear interval) when you get to zero .
+
+Starting <body> background-color:       element.style.backgroundColor = 'rgb(255, 255, 255)';
+After 0.5 second, background-color:     element.style.backgroundColor = 'rgb(254, 254, 254)';
+*/
+
 const body = document.getElementsByTagName('body')[0];
 let red = 255;
 let green = 255;
