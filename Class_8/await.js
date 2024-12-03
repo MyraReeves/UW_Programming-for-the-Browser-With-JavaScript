b38092191f7971133da9dd9d1c999e81c91c75d6
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-02 23:30:29 -0800

    Adds NY Times API example

--- Class_8/await.js
+++ Class_8/await.js
@@ -12,3 +12,31 @@
 //   .then(function(number) {
 //     console.log(number);
 //   });
+
+
+const fetchPromise = new Promise((resolve, reject) => {
+    const data = fetch('https://jsonplaceholder.typicode.com/todos/1');
+    resolve(data);
+    reject(data);
+});
+
+fetchPromise.then(response => response.json()).then(json => console.log(json)).catch(error => console.log("An error occurred!"));
+
+
+const fetchStarWarsData = new Promise((resolve, reject) => {
+    const starWarsData = fetch('https://swapi.dev/api/people/1/');
+    resolve(starWarsData);
+    reject(starWarsData);
+});
+
+fetchStarWarsData.then(response => response.json())
+.then(json => console.log(json));
+
+
+
+// NY Times best seller hardcover fetch:
+const myKey = 'ZvE9LRUyFK0a8nZZpNecEKGrtZ6vjSIr'
+fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${myKey}`)
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error));
